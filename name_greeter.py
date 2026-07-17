84b07284c9af20cf4278fd4a3e841c4a444d14c9
saiton-del <saitonnampaso09@gmail.com>
2026-07-17 07:44:36 -0600

    Update name_greeter.py

--- name_greeter.py
+++ name_greeter.py
@@ -1,1 +1,13 @@
+# name_greeter.py
 
+# Ask the user to enter their full name
+full_name = input("Please enter your full name: ")
+
+# Split the name into separate words
+name_parts = full_name.split()
+
+# Check if the user entered two or more names
+if len(name_parts) >= 2:
+    print("Hello,", name_parts[0] + "!")
+else:
+    print("Please enter your full name (first and last name).")
